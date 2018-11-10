93d9cef6fec6a8422ccd6a7599e277c1f433e8dd
Sushant Sahu <sushant87@gmail.com>
2018-11-10 09:53:52 +0530

    optionals

--- basics_1.playground/Contents.swift
+++ basics_1.playground/Contents.swift
@@ -4,3 +4,53 @@ import UIKit
 
 var str = "Hello People Whats up ?"
 print(str)
+
+var num = 42
+print("value of number is : \(num)")
+
+// Optionals
+
+var value: String?
+
+
+if value != nil {
+    print(value)
+}else {
+    print("empty")
+}
+
+// Forced unwrapping
+value = "Awesome"
+if value != nil {
+    print(value)
+    print(value!)
+}else {
+    print("empty")
+}
+
+
+// Automatic unwrapping
+var autovalue: String!
+autovalue = "this is automatic unwrapping"
+
+if autovalue != nil {
+    print(autovalue)
+}else {
+    print("empty")
+}
+
+// Optional binding
+var optunbind: String!
+optunbind = "this is an optional unbinding"
+
+if let actvalue = optunbind {
+    print("what is \(actvalue) ?")
+}else {
+    print("empty")
+}
+
+
+
+
+
+

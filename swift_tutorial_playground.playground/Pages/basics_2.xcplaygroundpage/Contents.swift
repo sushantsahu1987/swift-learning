29a881eff578823aad6cd7f2ecf5d1fbf0a33aed
Sushant Sahu <sushant87@gmail.com>
2018-11-10 09:59:58 +0530

    refactoring, tuples, constants

---
+++ swift_tutorial_playground.playground/Pages/basics_2.xcplaygroundpage/Contents.swift
@@ -0,0 +1,15 @@
+//: [Previous](@previous)
+
+import Foundation
+
+// Tuples
+
+var error = (401, "page not found")
+print("the values of the error tuple is")
+print("error code \(error.0), message : \(error.1)");
+
+// Constants
+let pi = 3.142
+print("the value of pi is \(pi)")
+
+

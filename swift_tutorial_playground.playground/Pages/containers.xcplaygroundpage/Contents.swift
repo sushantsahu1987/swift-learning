dbf7009a8c57c700c48f8eeb70db4a7a767443e5
Sushant Sahu <sushant87@gmail.com>
2018-11-10 21:50:05 +0530

    code update

--- swift_tutorial_playground.playground/Pages/containers.xcplaygroundpage/Contents.swift
+++ swift_tutorial_playground.playground/Pages/containers.xcplaygroundpage/Contents.swift
@@ -48,6 +48,37 @@ print(s)
 
 //  Intersection, Union, Subtracting
 
+var s1:Set = [10,12,13,14,56]
+var s2:Set = [15,12,14]
+var s3 = [17,19,25,12,90]
+print(s1.union(s2).sorted())
+print(s1.intersection(s2).sorted())
+
+// Dictionary
+
+var d1 = [Int: String]()
+d1[0] = "sushant"
+d1[2] = "khushboo"
+print(d1)
+
+var d2: [Int: String] = [1:"abc",2:"bdf"]
+print(d2)
+
+var cities:[String] = ["Bangalore","Chennai","Mumbai","Hyderabad","Belgavi"]
+var distance:[Int] = [100,200,300,400]
+let citydistance = Dictionary(uniqueKeysWithValues: zip(cities, distance))
+print(citydistance)
+var closecities = citydistance.filter{ $0.value > 200}
+print(closecities)
+
+var grpcities = Dictionary(grouping: cities) {$0.first!}
+print(grpcities)
+
+// Accessing dictionary
+
+
+// Modifying dictionary
+
 
 
 
